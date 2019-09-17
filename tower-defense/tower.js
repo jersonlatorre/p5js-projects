58fa2d41c1b180b9d5a2c1f58dc5d7ba12b69aaf
Jerson La Torre <jerson.latorre@gmail.com>
2019-09-17 17:11:03 -0500

    using states & SLEEPING state added

--- tower-defense/tower.js
+++ tower-defense/tower.js
@@ -1,14 +1,20 @@
 class Tower {
 	attackRadius = 250
-	actionRadius = 60
+	actionRadius = 50
 	bullets = []
 	position
 	basePosition
 	t = 0
 	velocity = new p5.Vector(0, 0)
 	steer
-	maxForce = 0.5
-	maxSteer = 0.5
+	maxForce = 1
+	maxSteer = 0.9
+
+	zzzs = []
+	timerZzz = 0
+
+	State = { SLEEPING: 0, ATTACKING: 1, RETURNING: 2 }
+	state = this.State.SLEEPING
 
 	constructor(x, y) {
 		this.position = new p5.Vector(x, y)
@@ -16,19 +22,79 @@ class Tower {
 	}
 
 	draw() {
-		let nearest = this.getNearestNpc()
-		// this.steer = new p5.Vector(0, 0)
-		this.steer = SteeringBehaviours.arrive(this, this.basePosition, this.actionRadius)
-		if (nearest != null) {
-			this.steer.add(SteeringBehaviours.seek(this, nearest.position))
-			if (this.t % 15 == 0) {
-				let bullet = new Bullet(this.position.x, this.position.y, nearest)
-				this.bullets.push(bullet)
+		switch (this.state) {
+			case this.State.SLEEPING: {
+				if (this.timerZzz > 0.3) {
+					this.timerZzz = 0
+					this.spawnZzz()
+				}
+
+				this.timerZzz += 0.01
+
+				noStroke()
+				fill('rgba(100, 180, 0, 1)')
+				circle(this.position.x, this.position.y, 20)
+				fill('rgba(0, 0, 0, 0.2)')
+				circle(this.position.x, this.position.y, 10)
+
+				if (this.getNearestNpc() != null) {
+					this.state = this.State.ATTACKING
+				}
+
+				break
+			}
+
+			case this.State.ATTACKING: {
+				let nearest = this.getNearestNpc()
+
+				if (nearest != null) {
+					this.steer = new p5.Vector(0, 0)
+					this.steer.add(SteeringBehaviours.arrive(this, this.basePosition, this.actionRadius))
+					this.steer.add(SteeringBehaviours.seek(this, nearest.position))
+
+					if (this.t % 15 == 0) {
+						let bullet = new Bullet(this.position.x, this.position.y, nearest)
+						this.bullets.push(bullet)
+					}
+
+					this.t += 1
+				} else {
+					this.state = this.State.RETURNING
+				}
+
+				this.velocity.add(this.steer)
+				this.position.add(this.velocity)
+
+				noStroke()
+				fill('rgba(100, 180, 0, 1)')
+				circle(this.position.x, this.position.y, 20)
+				fill('rgba(0, 0, 0, 0.2)')
+				circle(this.position.x, this.position.y, 10)
+
+				break
+			}
+
+			case this.State.RETURNING: {
+				this.steer = SteeringBehaviours.arrive(this, this.basePosition, this.actionRadius)
+				this.velocity.add(this.steer)
+				this.position.add(this.velocity)
+
+				if (this.velocity.mag() < 0.1) {
+					this.state = this.State.SLEEPING
+				}
+
+				if (this.getNearestNpc() != null) {
+					this.state = this.State.ATTACKING
+				}
+
+				noStroke()
+				fill('rgba(100, 180, 0, 1)')
+				circle(this.position.x, this.position.y, 20)
+				fill('rgba(0, 0, 0, 0.2)')
+				circle(this.position.x, this.position.y, 10)
+				break
 			}
 		}
-
-		this.velocity.add(this.steer)
-		this.position.add(this.velocity)
 
 		this.bullets.forEach((bullet, i) => {
 			bullet.draw()
@@ -37,13 +103,17 @@ class Tower {
 			}
 		})
 
-		this.t += 1
+		this.zzzs.forEach((zzz, i) => {
+			zzz.draw()
+			if (zzz.isDead) {
+				this.zzzs.splice(i, 1)
+			}
+		})
+	}
 
-		noStroke()
-		fill('rgba(100, 180, 0, 1)')
-		circle(this.position.x, this.position.y, 20)
-		fill('rgba(0, 0, 0, 0.2)')
-		circle(this.position.x, this.position.y, 10)
+	spawnZzz() {
+		let zzz = new Zzz(this.position.x, this.position.y)
+		this.zzzs.push(zzz)
 	}
 
 	getNearestNpc() {
