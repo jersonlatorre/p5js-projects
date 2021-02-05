566ef783904ca7ea422d9f968894c5c705817199
Jerson La Torre <jerson.latorre@gmail.com>
2021-02-05 02:32:16 -0500

    update

--- 2021-01_test-ml5/main.js
+++ 2021-01_test-ml5/main.js
@@ -8,84 +8,84 @@ ml5 Example
 Creating a regression extracting features of MobileNet. Build with p5js.
 === */
 
-let featureExtractor;
-let regressor;
-let video;
-let loss;
-let slider;
-let samples = 0;
-let positionX = 140;
+let featureExtractor
+let regressor
+let video
+let loss
+let slider
+let samples = 0
+let positionX = 140
 
 function setup() {
-  createCanvas(340, 280);
+  createCanvas(340, 280)
   // Create a video element
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO)
   // Append it to the videoContainer DOM element
-  video.hide();
+  video.hide()
   // Extract the features from MobileNet
-  featureExtractor = ml5.featureExtractor("MobileNet", modelReady);
+  featureExtractor = ml5.featureExtractor('MobileNet', modelReady)
   // Create a new regressor using those features and give the video we want to use
-  regressor = featureExtractor.regression(video, videoReady);
+  regressor = featureExtractor.regression(video, videoReady)
   // Create the UI buttons
-  setupButtons();
+  setupButtons()
 }
 
 function draw() {
-  image(video, 0, 0, 340, 280);
-  noStroke();
-  fill(255, 0, 0);
-  rect(positionX, 120, 50, 50);
+  image(video, 0, 0, 340, 280)
+  noStroke()
+  fill(255, 0, 0)
+  rect(positionX, 120, 50, 50)
 }
 
 // A function to be called when the model has been loaded
 function modelReady() {
-  select("#modelStatus").html("Model loaded!");
+  select('#modelStatus').html('Model loaded!')
 }
 
 // A function to be called when the video has loaded
 function videoReady() {
-  select("#videoStatus").html("Video ready!");
+  select('#videoStatus').html('Video ready!')
 }
 
 // Classify the current frame.
 function predict() {
-  regressor.predict(gotResults);
+  regressor.predict(gotResults)
 }
 
 // A util function to create UI buttons
 function setupButtons() {
-  slider = select("#slider");
+  slider = select('#slider')
   // When the Dog button is pressed, add the current frame
   // from the video with a label of "dog" to the classifier
-  select("#addSample").mousePressed(function() {
-    regressor.addImage(slider.value());
-    select("#amountOfSamples").html((samples += 1));
-  });
+  select('#addSample').mousePressed(function() {
+    regressor.addImage(slider.value())
+    select('#amountOfSamples').html((samples += 1))
+  })
 
   // Train Button
-  select("#train").mousePressed(function() {
+  select('#train').mousePressed(function() {
     regressor.train(function(lossValue) {
       if (lossValue) {
-        loss = lossValue;
-        select("#loss").html(`Loss: ${loss}`);
+        loss = lossValue
+        select('#loss').html(`Loss: ${loss}`)
       } else {
-        select("#loss").html(`Done Training! Final Loss: ${loss}`);
+        select('#loss').html(`Done Training! Final Loss: ${loss}`)
       }
-    });
-  });
+    })
+  })
 
   // Predict Button
-  select("#buttonPredict").mousePressed(predict);
+  select('#buttonPredict').mousePressed(predict)
 }
 
 // Show the results
 function gotResults(err, result) {
   if (err) {
-    console.error(err);
+    console.error(err)
   }
   if (result && result.value) {
-    positionX = map(result.value, 0, 1, 0, width);
-    slider.value(result.value);
-    predict();
+    positionX = map(result.value, 0, 1, 0, width)
+    slider.value(result.value)
+    predict()
   }
 }
