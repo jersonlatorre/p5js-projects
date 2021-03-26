3d8627ef9841f4ea5893934ce6a259fe98a6e248
Jerson La Torre <jerson.latorre@gmail.com>
2021-03-26 15:56:43 -0500

    update

--- 2020_succion/main.js
+++ 2020_succion/main.js
@@ -1,74 +1,38 @@
-let global = {
-  speed: 0,
-  delayFactor: 0,
-  size: 900,
-  numLines: 10,
-  lineWidth: 2
-}
+let speed = 0
+let delayFactor = 0
+let size = 100
+let numLines = 3
 
 let time = 0
 let simplex = new SimplexNoise()
 let mousePosition
-let toDelayFactor = 0
-let toSize = 0
-let toLineWidth = 0
 
 function setup() {
-  createCanvas(1080, 1080)
+  createCanvas(windowWidth, windowHeight)
   colorMode(HSB)
-
   mousePosition = createVector()
-
-  let pane = new Tweakpane({
-    title: 'parameters'
-  })
-
-  pane.addInput(global, 'speed', {
-    min: 0,
-    max: 0.01
-  })
-
-  pane.addInput(global, 'delayFactor', {
-    min: 0,
-    max: 0.2
-  })
-
-  pane.addInput(global, 'numLines', {
-    min: 3,
-    max: 30,
-    step: 1
-  })
-
-  document.querySelector('.tp-dfwv').style.opacity = 0.8
-  document.querySelector('.tp-dfwv').style.top = 'auto'
-  document.querySelector('.tp-dfwv').style.bottom = '15px'
-  document.querySelector('.tp-dfwv').style.right = parseInt(20 + 0.5 * (window.innerWidth - 1080)) + 'px'
 }
 
 function draw() {
   blendMode(BLEND)
   background('rgba(255, 255, 255, 0.2)')
   background('rgba(0, 0, 0, 1)')
-  // blendMode(ADD)
-  time += global.speed
 
-  toDelayFactor = lerp(toDelayFactor, global.delayFactor, 0.05)
-  toSize = lerp(toSize, global.size, 0.05)
-  toLineWidth = lerp(toLineWidth, global.lineWidth, 0.05)
+  time += speed
 
-  let rx = width / 2 + 500 * simplex.noise2D(millis() / 3000, 100)
-  let ry = height / 2 + 500 * simplex.noise2D(millis() / 3000, 200)
+  let rx = width / 2 + 100 * simplex.noise2D(millis() / 3000, 100)
+  let ry = height / 2 + 100 * simplex.noise2D(millis() / 3000, 200)
 
   mousePosition = createVector(rx, ry)
 
-  for (let i = 0; i < global.numLines; i++) {
+  for (let i = 0; i < numLines; i++) {
     let points = []
-    let N = 100
+    let N = 50
 
     for (let j = 0; j <= N; j += 1) {
       let t = j / N
-      let angle = 2 * PI * simplex.noise2D(time - toDelayFactor * t, i)
-      let r = toSize + toSize * 0.4 * simplex.noise2D(time - toDelayFactor * t, i + 1)
+      let angle = 2 * PI * simplex.noise2D(time - delayFactor * t, i)
+      let r = size + size * 2 * simplex.noise2D(time - delayFactor * t, i + 1)
       let x1 = width / 2 + r * cos(angle)
       let y1 = height / 2 + r * sin(angle)
       let x = lerp(x1, mousePosition.x, t)
@@ -82,11 +46,15 @@ function draw() {
       let p2 = points[i + 1]
 
       noFill()
-      strokeWeight(map(i, 0, N - 1, 0, 2))
-      stroke(map(i, 0, N - 1, 360, 0), 100, 100)
+      strokeWeight(map(i, 0, N - 1, 1, 0))
+      stroke(map(i, 0, N - 1, 360, 0), 0, 255)
 
-      line(p1.x, p1.y, p2.x, p2.y)
-      // circle(p1.x, p1.y, map(i, 0, N - 1, 0, 80))
+      // line(p1.x, p1.y, p2.x, p2.y)
+      circle(p1.x, p1.y, map(i, 0, N - 1, 0, min(width, height) * 0.8))
     }
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
--- 2021_dropping/src/main.js
+++ 2021_dropping/src/main.js
@@ -1,35 +1,26 @@
 let agents = []
-let font
 let fps
-
-function preload() {
-  font = loadFont('assets/Hack-Bold.ttf')
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  textFont(font)
-  frameRate(60)
   colorMode(HSB)
-  background('black')
+  rectMode(CORNER)
+  textAlign(CENTER, CENTER)
 
-  for (let i = 0; i < 600; i++) {
+  background('white')
+
+  for (let i = 0; i < 300; i++) {
     let agent = new Agent()
     agent.position = new p5.Vector(random(width), random(height))
     agents.push(agent)
   }
 
-  fps = new Fps()
+  // fps = new Fps()
 }
 
 function draw() {
-  // translate(-width / 2, -height / 2)
-  
-  fill(0, 0.01)
+  fill(255, 0.04)
   rect(0, 0, width, height)
-
-  rectMode(CENTER)
-  textAlign(CENTER, CENTER)
 
   agents.forEach((agent) => {
     agent.draw()
