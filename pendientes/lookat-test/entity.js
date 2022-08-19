8fc7efac9bd4d38e62e9587eb26805275e8d7ca1
Jerson La Torre <jerson.latorre@gmail.com>
2022-08-18 23:04:26 -0500

    add: new project added

---
+++ pendientes/lookat-test/entity.js
@@ -0,0 +1,30 @@
+class Entity {
+	constructor(x, y) {
+		this.position = createVector(x, y)
+	}
+	draw() {
+		let distance = dist(mouseSmoothX, mouseSmoothY, this.position.x, this.position.y)
+		fill('red')
+		noStroke()
+		let angle = map(distance, 0, diagonal, 2 * PI, 0)
+		angle = atan2(mouseSmoothY - this.position.y, mouseSmoothX - this.position.x)
+		let factor = map(distance, 0, diagonal, 1, 0)
+		let weight = map(distance, 0, diagonal, 4, 0)
+		push()
+		translate(this.position.x, this.position.y)
+		rotate(angle)
+		// -----------------
+		// strokeWeight(weight)
+		// stroke('rgba(255, 255, 255, 0.5)')
+		// line(-size, 0, size, 0)
+		fill('white')
+		noStroke()
+		circle(0, 0, cell_size * factor * 0.9)
+		stroke('black')
+		strokeWeight(3 * factor)
+		line(-cell_size * 0.2 * factor, 0, cell_size * 0.2 * factor, 0)
+		// ----------------
+		pop()
+		console.log('hola')
+	}
+}
---
+++ pendientes/lookat-test/main.js
@@ -0,0 +1,35 @@
+let entities = []
+let cell_size = 40
+let diagonal
+let mouseSmoothX = 0, mouseSmoothY = 0
+
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+	let nw = int(windowWidth / cell_size)
+	let nh = int(windowHeight / cell_size)
+	let offsetx = (windowWidth - cell_size * nw) / 2
+	let offsety = (windowHeight - cell_size * nh) / 2
+	diagonal = sqrt(windowWidth * windowWidth + windowHeight * windowHeight)
+
+	for (let i = 0; i <= nw; i++) {
+		for (let j = 0; j <= nh; j++) {
+			entities.push(new Entity(i * cell_size + offsetx, j * cell_size + offsety))
+		}
+	}
+}
+
+function draw() {
+	background('black')
+	mouseSmoothX = lerp(mouseSmoothX, mouseX, 0.06)
+	mouseSmoothY = lerp(mouseSmoothY, mouseY, 0.06)
+	// console.log(mouseSmoothX, mouseSmoothY)
+	fill('red')
+	// circle(mouseSmoothX, mouseSmoothY, 30)
+	entities.forEach(entity => {
+		entity.draw()
+	})
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
+}
